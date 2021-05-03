3225851a94677b5ab1ec48a59058b9037ed6dd93
Mohammadreza kamyab <83444030+Subversion0@users.noreply.github.com>
2021-05-03 14:14:42 +0430

    Question 3 Project euler

--- Q3_Largest prime factor/Q3 Largest prime factor .py
+++ Q3_Largest prime factor/Q3 Largest prime factor .py
@@ -1,15 +1,29 @@
+#! /usr/bin/python3
+
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143 ?
+from math import sqrt, ceil
 
-a = 2;
-number = 600851475143;
-reverse = 0;
+def main():
+    print(Largest_Prime(60))
 
-for a in range(a , number):
-
-    while (number % a == 0):
-        if reverse != a:
-            reverse = a
-            print(reverse)    
-        number = number / a
+def Largest_Prime(n):
+    # since 2 is the only even prime
+    if n == 1: return print('there is no prime numbers')
+    if n < 1: return print('the number you have given is not a Natural Number')
+    
+    counter = 2
+    while (n % counter) == 0:
+        n = n / counter
+        prime = counter
+    
+    counter = 3
+    while counter <= n:
+        while (n % counter) == 0:
+            n = n / counter
+            prime = counter
+        counter += 2
+    
+    return prime
+if __name__ == "__main__" : main()
     
